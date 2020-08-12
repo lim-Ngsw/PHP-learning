8833e7f770410143e1dd56448648d92cb3ba94b3
lim-Ngsw <62245738+lim-Ngsw@users.noreply.github.com>
2020-08-12 16:34:29 +0900

    new learning!!

--- html/index.php
+++ html/index.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <title>POSTのサンプル</title>
-  </head>
-  <body>
-    <?php
-      //commentがPOSTされているなら
-      if(isset($_POST["comment"])){
-        //エスケープしてから表示
-        $comment = htmlspecialchars($_POST["comment"]);
-        print("あなたのコメントは「 ${comment} 」です。");
-      } else {
-    ?>
-        <p>コメントしてください。</p>
-        <form method="POST" action="index.php">
-          <input name="comment" />
-          <input type="submit" value="送信" />
-        </form>
-    <?php
-      }
-    ?>
-  </body>
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<!-- ここにプログラムを記述します -->
+</pre>
+</main>
+</body>    
 </html>
