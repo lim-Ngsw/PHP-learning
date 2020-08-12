27acf94a4bba7eb73505c6ad9391086601c11659
lim-Ngsw <62245738+lim-Ngsw@users.noreply.github.com>
2020-08-12 16:28:45 +0900

    learning!

--- html/function.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 引数の文字列を2回表示する関数
-function double_print($text){
-  print($text . $text);
-}
-
-double_print("a");
-double_print("bc");
-?>
--- html/ifsyntax.php
+++
@@ -1,8 +0,0 @@
-<?php
-$a = 5;
-if($a == 3){
-  print("$a is 3");
- }else{
-  print("$a is not 3");
-}
-?>
